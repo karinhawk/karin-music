818df6e9c07f7b8933c166f0d474c4ade6f68c4e
Karin Hawksworth <karin.hawksworth@gmail.com>
2024-08-26 20:55:51 +0100

    new pic

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import pic from "../../../public/images/icon2.png"
+import pic from "../../../public/images/icon5.png"
 import styles from "./index.module.css"
 
 export default function Contact() {
