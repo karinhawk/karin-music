231a2a1a01b5671f7a905d0d0216ac94c6de529e
Karin Hawksworth <karin@MacBook-Pro.local>
2025-02-06 12:54:36 +0000

    add player to home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import pic from "../../public/images/icon4.png";
 import styles from "./index.module.css";
 import Link from "next/link";
+import SoundCloudEmbedded from "@/components/embeddedPlayer";
 
 export default function Home() {
   return (
@@ -11,6 +12,16 @@ export default function Home() {
         <p>
           {`Karin Hawksworth is a young London-based composer, cellist, and site reliability engineer.`}
         </p>
+        <div className={styles.buffer}>
+          <p>
+          {`Listen to Badlands`}
+          </p>
+        </div>
+        <SoundCloudEmbedded
+            url={
+              "https://soundcloud.com/karinhawksworth/badlands-2024-virtual-playback?si=bf84de4949e64cc89eb56c0c810a84a9&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+            }
+          />
         <Link href="/music">
           <p className={styles.showme}>My Pieces</p>
         </Link>
