ca7f68b43cf7462c8db20eb54b1d7cd9396d19bc
Karin Hawksworth <karin@MacBook-Pro.local>
2026-02-27 14:53:26 +0000

    update footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.css";
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <nav>@ Karin Hawksworth 2024</nav>
+      <nav>@ Karin Hawksworth 2026</nav>
     </div>
   );
 }
